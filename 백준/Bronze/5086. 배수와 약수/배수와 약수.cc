06885541098fba584b422ec04530cdf3a56c97f1
vikira <seowon2191@gmail.com>
2025-08-04 14:40:27 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	
+	while (1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) break;
+		if (b % a == 0) cout << "factor" << '\n';
+		else if (a % b == 0) cout << "multiple" << '\n';
+		else cout << "neither" << '\n';
+	}
+
+    return 0;
+}
