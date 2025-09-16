6087cb39ee1dd8d82c247383f08abb316c022f46
vikira <seowon2191@gmail.com>
2025-09-16 20:08:41 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int house[1001][3];
+int main() {
+    int n, cost[3];
+    cin >> n;
+    house[0][0] = 0;
+    house[0][1] = 0;
+    house[0][2] = 0;
+    for (int i = 1; i <= n; ++i) {
+        cin >> cost[0] >> cost[1] >> cost[2];
+
+        house[i][0] = min(house[i - 1][1], house[i - 1][2]) + cost[0];
+        house[i][1] = min(house[i - 1][0], house[i - 1][2]) + cost[1];
+        house[i][2] = min(house[i - 1][1], house[i - 1][0]) + cost[2];
+    }
+    cout << min(house[n][2], min(house[n][0], house[n][1]));
+
+    return 0;
+}
