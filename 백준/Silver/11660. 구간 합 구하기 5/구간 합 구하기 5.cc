4011a640f247ebe91c52da65a0f8f57eddd796f0
vikira <seowon2191@gmail.com>
2025-10-06 17:26:53 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 120 ms, Memory: 18596 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<long long>> v(n + 1, vector<long long>(n + 1, 0));
+	vector<vector<long long>> dp(n + 1, vector<long long>(n + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> v[i][j];
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + v[i][j];
+        }
+    }
+
+    for (int q = 0; q < m; q++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        long long sum = dp[x2][y2]
+            - dp[x1 - 1][y2]
+            - dp[x2][y1 - 1]
+            + dp[x1 - 1][y1 - 1];
+
+        cout << sum << '\n';
+    }
+
+
+	return 0;
+}
