2dcc28e91be167bfbcd66404f1d9789a93db851a
vikira <seowon2191@gmail.com>
2025-10-20 08:38:01 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) cout << i << '\n';
+
+    return 0;
+}
