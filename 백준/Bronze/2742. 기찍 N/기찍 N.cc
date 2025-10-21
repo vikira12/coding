352d96031ba0f5fa76bed03959819dcd0ab35f29
vikira <seowon2191@gmail.com>
2025-10-21 22:01:16 +0900

    [Bronze V] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    for (int i = n; i > 0; i--) cout << i << '\n';
+
+    return 0;
+}
