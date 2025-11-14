e248c344d357f800ceec5702c41c6af0f9c1edad
vikira <seowon2191@gmail.com>
2025-11-14 23:44:36 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout << left;
+    cout << setw(15) << "SHIP NAME"
+        << setw(15) << "CLASS"
+        << setw(11) << "DEPLOYMENT"
+        << setw(10) << "IN SERVICE" << "\n";
+
+    cout << setw(15) << "N2 Bomber"
+        << setw(15) << "Heavy Fighter"
+        << setw(11) << "Limited"
+        << setw(10) << "21" << "\n";
+
+    cout << setw(15) << "J-Type 327"
+        << setw(15) << "Light Combat"
+        << setw(11) << "Unlimited"
+        << setw(10) << "1" << "\n";
+
+    cout << setw(15) << "NX Cruiser"
+        << setw(15) << "Medium Fighter"
+        << setw(11) << "Limited"
+        << setw(10) << "18" << "\n";
+
+    cout << setw(15) << "N1 Starfighter"
+        << setw(15) << "Medium Fighter"
+        << setw(11) << "Unlimited"
+        << setw(10) << "25" << "\n";
+
+    cout << setw(15) << "Royal Cruiser"
+        << setw(15) << "Light Combat"
+        << setw(11) << "Limited"
+        << setw(10) << "4" << "\n";
+
+    return 0;
+}
