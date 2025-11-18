a90d5f6b7769a514d181311dab54a8165a1581e4
vikira <seowon2191@gmail.com>
2025-11-18 17:26:45 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+
+    priority_queue<int> q;
+    vector<int> v;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+
+        if (num == 0) {
+            if (q.size() == 0) v.push_back(0);
+            else {
+                v.push_back(q.top());
+                q.pop();
+            }
+        }
+        else q.push(num);
+    }
+
+    for(int i = 0 ; i < v.size(); i++) cout << v[i] << '\n';
+
+    return 0;
+}
