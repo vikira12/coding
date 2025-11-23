62219d5dd6bb0fcae796847975ade04e8e9572dc
vikira <seowon2191@gmail.com>
2025-11-23 13:49:43 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    long long a, b;
+    cin >> a >> b;
+
+    if (a == b) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
